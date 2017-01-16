c29bec0e78ae04f6661274b7cc59edc1157907bf
Kenny <k.bienaime@hotmail.com>
2017-01-15 21:45:16 -0500

    Font p.130_Example of a Select statement

---
+++ Chapter 7/Font_SelectClause.aspx.vb
@@ -0,0 +1,13 @@
+﻿
+Partial Class Font_SelectClause
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim q = From f In System.Drawing.FontFamily.Families
+                Select f.Name
+        For Each fnt As String In q
+            Response.Write(fnt & “<br />”)
+        Next
+
+    End Sub
+End Class
