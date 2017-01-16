cbbccd0797c99f2b33cdccdfe9bbb82fde60e37d
Kenny <k.bienaime@hotmail.com>
2017-01-15 23:04:44 -0500

    Average Example p.136

---
+++ Chapter 7/Average Example.aspx.vb
@@ -0,0 +1,12 @@
+﻿
+Partial Class Average_Example
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim dc As New NWDataClassesDataContext
+        Dim q = (From p In dc.Products
+                 Select p.UnitPrice).Average()
+        Label1.Text = "The unit price is " &
+        Format(q, "C")
+    End Sub
+End Class
