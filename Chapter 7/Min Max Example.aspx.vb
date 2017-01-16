3f190d5be53d1ff3bd3ce66a3e56f9270df21131
Kenny <k.bienaime@hotmail.com>
2017-01-15 23:12:10 -0500

    Min max Example p.136

---
+++ Chapter 7/Min Max Example.aspx.vb
@@ -0,0 +1,14 @@
+﻿
+Partial Class Min_Max_Example
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim dc As New NWDataClassesDataContext
+        Dim q = From p In dc.Products
+                Select p.UnitPrice
+        Label1.Text = "The cheapest item is " &
+        Format(q.Min, "C") &
+        " and the most expensive is " &
+        Format(q.Max, "C")
+    End Sub
+End Class
