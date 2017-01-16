d24c708b89bb16d8929fa5d97aaa7cc09d982412
Kenny <k.bienaime@hotmail.com>
2017-01-15 22:46:15 -0500

    Example Filtering Date Time p.134

---
+++ Chapter 7/Filtering_dates_and_times.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Filtering_dates_and_times.aspx.vb" Inherits="Filtering_dates_and_times" %>
+<%@ Import Namespace="System.Linq" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+        </asp:CheckBoxList>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Chapter 7/Filtering_dates_and_times.aspx.vb
@@ -0,0 +1,19 @@
+﻿
+Partial Class Filtering_dates_and_times
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim cinfo As New System.Globalization.CultureInfo("en-CA")
+        Dim dt(2) As Date
+        dt(0) = Date.Parse("December 24, 2007", cinfo)
+        dt(1) = Date.Parse("May 22, 2008", cinfo)
+        dt(2) = Date.Parse("February 10, 2008", cinfo)
+        Dim q = From d In dt
+                Where d > DateTime.ParseExact _
+                ("January 13, 2008", "D", cinfo)
+                Select d.ToLongDateString
+        CheckBoxList1.DataSource = q
+        CheckBoxList1.DataBind()
+
+    End Sub
+End Class
