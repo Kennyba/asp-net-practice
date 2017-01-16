4c36f5405b10f78b0993586364faf286d4b48fb0
Kenny <k.bienaime@hotmail.com>
2017-01-15 23:25:00 -0500

    Skip Take Example

---
+++ Chapter 7/Skip and Take Example.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Skip and Take Example.aspx.vb" Inherits="Skip_and_Take_Example" %>
+<%@ Import Namespace="System.Linq" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Chapter 7/Skip and Take Example.aspx.vb
@@ -0,0 +1,30 @@
+﻿
+Partial Class Skip_and_Take_Example
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            GetData()
+        End If
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GetData()
+    End Sub
+    Protected Sub GetData()
+        Dim intPos As Integer = 0
+        If IsNothing(ViewState("pos")) Then
+            ViewState("pos") = 0
+        Else
+            intPos = Convert.ToInt32(ViewState("pos")) + 5
+            ViewState("pos") = intPos
+        End If
+        Dim q =
+        From c In
+        System.Globalization.CultureInfo.GetCultures(2)
+        Select c.EnglishName
+        Order By EnglishName Skip (intPos) Take (5)
+        GridView1.DataSource = q
+        GridView1.DataBind()
+    End Sub
+End Class
