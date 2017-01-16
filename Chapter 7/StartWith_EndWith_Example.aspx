26cbb8e0010662f3aa9f4a4c69a5d67754de12c0
Kenny <k.bienaime@hotmail.com>
2017-01-15 22:31:45 -0500

    Start with End with Example p.133

---
+++ Chapter 7/StartWith_EndWith_Example.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="StartWith_EndWith_Example.aspx.vb" Inherits="StartWith_EndWith_Example" %>
+<%@ Import Namespace="System.Linq" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:BulletedList ID="BulletedList1" runat="server">
+        </asp:BulletedList>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Chapter 7/StartWith_EndWith_Example.aspx.vb
@@ -0,0 +1,19 @@
+﻿
+Partial Class StartWith_EndWith_Example
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim names As String() =
+        {"Elaine", "Brenda", "Julie", "Jaclyn"}
+        Dim q = From s In names
+                Where
+                s.StartsWith("j",
+                StringComparison.CurrentCultureIgnoreCase) _
+                Or
+                s.EndsWith("e",
+                StringComparison.CurrentCultureIgnoreCase)
+
+        BulletedList1.DataSource = q
+        BulletedList1.DataBind()
+    End Sub
+End Class
