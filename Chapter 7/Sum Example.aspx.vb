926727713097297b7d3452b8a5bf4c90bb4e34e4
Kenny <k.bienaime@hotmail.com>
2017-01-15 23:07:46 -0500

    Sum Example p.136

---
+++ Chapter 7/Sum Example.aspx.vb
@@ -0,0 +1,12 @@
+﻿
+Partial Class Sum_Example
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim dc As New NWDataClassesDataContext
+        Dim q = (From p In dc.Products
+                 Select p.UnitPrice).Sum()
+        Label1.Text = “The total is “ &
+        Format(q, “C”)
+    End Sub
+End Class
