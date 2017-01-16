ebea2955ebf4ecc8c46b455367e94ec3c4dc6fb5
Kenny <k.bienaime@hotmail.com>
2017-01-15 22:09:42 -0500

    Contain Example p.132

---
+++ Chapter 7/Contains_Example.aspx.vb
@@ -0,0 +1,13 @@
+﻿
+Partial Class Contains_Example
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim dc As New NWDataClassesDataContext
+        Dim q = From p In dc.Products
+                Select p
+                Where p.ProductName.Contains("co")
+        GridView1.DataSource = q
+        GridView1.DataBind()
+    End Sub
+End Class
