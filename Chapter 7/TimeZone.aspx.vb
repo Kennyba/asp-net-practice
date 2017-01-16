680b385a333f3334f4b44b15c00a9eeedcd25761
Kenny <k.bienaime@hotmail.com>
2017-01-15 21:29:31 -0500

    Example Time zone p.102

---
+++ Chapter 7/TimeZone.aspx.vb
@@ -0,0 +1,15 @@
+﻿
+Partial Class TimeZone
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Dim q = From tz In System.TimeZoneInfo.GetSystemTimeZones
+        For Each tz As TimeZoneInfo In q
+            Response.Write(tz.DisplayName & “<br />”)
+        Next
+        GridView1.DataSource = q
+        GridView1.DataBind()
+
+    End Sub
+End Class
