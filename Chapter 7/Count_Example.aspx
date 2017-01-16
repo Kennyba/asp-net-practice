1982ca329a7027bc41b909979993381550708bbc
Kenny <k.bienaime@hotmail.com>
2017-01-15 22:57:02 -0500

    Count Example p.135

---
+++ Chapter 7/Count_Example.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Count_Example.aspx.vb" Inherits="Count_Example" %>
+<%@ Import Namespace="System.Linq" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Chapter 7/Count_Example.aspx.vb
@@ -0,0 +1,18 @@
+﻿
+Partial Class Count_Example
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Try
+            Dim q = From f In New System.IO.DirectoryInfo _
+            (Server.MapPath("~")).GetFiles()
+                    Select f
+                    Where f.Extension = ".aspx"
+            Label1.Text = "ASPX count: " & q.Count().ToString()
+            GridView1.DataSource = q
+            GridView1.DataBind()
+        Catch ex As Exception
+            Label1.Text = "Not allowed to do that!"
+        End Try
+    End Sub
+End Class
